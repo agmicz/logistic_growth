f15e7e93dd5d8e4852bccea87fe330ecbad1bbce
agata czernecka <agata.czernecka@worc.ox.ac.uk>
2023-11-07 14:32:44 +0000

    slay

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,27 +1,31 @@
 #Script to plot data and model
+# assess the fit of the model to your data graphically 
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("/cloud/project/experiment1.csv")
 
 logistic_fun <- function(t) {
   
   N <- (N0*K*exp(r*t))/(K-N0+N0*exp(r*t))
   
-  return(N)
-  
-}
+  return(N)}
 
-N0 <- ??? #
-  
-r <- ??? #
-  
-K <- ??? #
 
-ggplot(aes(???,???), data = growth_data) +
+N0 <-  7.185544 #
+  
+r <- 0.009522 #
+  
+K <-   5.979e+10  #
+
+ggplot(aes(t, N), data = growth_data) +
   
   geom_function(fun=logistic_fun, colour="red") +
   
-  geom_point()
+  geom_point() +
 
-  #scale_y_continuous(trans='log10')
+  scale_y_continuous(trans='log10')
 
+#makes list of packages used 
+sink(file = "package-versions.txt")
+sessionInfo()
+sink()
 
